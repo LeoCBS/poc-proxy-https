027d448f42a09aac2cecf2c8d0f638d1d7ac607b
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2016-11-21 12:45:51 -0200

    add main

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+var (
+	proxy    string
+	user     string
+	password string
+	dest     string
+)
+
+func main() {
+
+	flag.StringVar(&proxy, "proxy", "", "provide proxy URL: IP:PORT")
+	flag.StringVar(&user, "user", "", "provide proxy user")
+	flag.StringVar(&password, "password", "", "provide proxy password")
+	flag.StringVar(&dest, "dest", "", "provide URL to access")
+	flag.Parse()
+
+	req, _ := http.NewRequest("GET", dest, nil)
+	//req.Header.Set("Host", "www.google.com.br")
+
+	proxyURL := url.URL{
+		Scheme: "https",
+		Host:   proxy}
+
+	transport := &http.Transport{
+		Proxy:           http.ProxyURL(&proxyURL),
+		TLSClientConfig: &tls.Config{},
+	}
+	client := &http.Client{Transport: transport}
+	req.RequestURI = ""
+
+	auth := fmt.Sprintf("%s:%s", user, password)
+	basic := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
+	req.Header.Add("Proxy-Authorization", basic)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("erro: %s", err)
+	}
+	fmt.Printf("code: %s", resp.StatusCode)
+	htmlData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(os.Stdout, string(htmlData))
+}
